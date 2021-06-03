a29f36bb919f549bf56dbc832b156023cb7b6c1f
junyeon1997 <junyeon2012@gmail.com>
2021-06-03 12:52:21 +0900

    Add insertionSort method

--- src/two_InsertionSort_problem/InsertionSort.java
+++ src/two_InsertionSort_problem/InsertionSort.java
@@ -2,7 +2,23 @@ package two_InsertionSort_problem;
 
 public class InsertionSort {
 
-    public void static insertionSort(int[] arr, int count){
+    public void insertionSort(int[] arr, int count){
+
+        int i = 0;
+        int j = 0;
+        int temp = 0;
+
+        for(i = 1; j < count; i++){
+            temp = arr[i];
+            j = i;
+            while ((j > 0) && arr[j-1] > temp) {
+                arr[j] = arr[j-1];
+                j = j-1;
+            }
+            arr[j] = temp;
+
+            System.out.println("반복 -" + i);
+        }
 
 
     }
