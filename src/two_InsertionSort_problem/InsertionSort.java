f08e708f71ae48a01db9d137616e019a0fb503c8
junyeon1997 <junyeon2012@gmail.com>
2021-06-03 13:03:55 +0900

    Add printSort method

--- src/two_InsertionSort_problem/InsertionSort.java
+++ src/two_InsertionSort_problem/InsertionSort.java
@@ -2,13 +2,13 @@ package two_InsertionSort_problem;
 
 public class InsertionSort {
 
-    public void insertionSort(int[] arr, int count){
+    public static void insertionSort(int[] arr, int count){
 
         int i = 0;
         int j = 0;
         int temp = 0;
 
-        for(i = 1; j < count; i++){
+        for(i = 1; i < count; i++){
             temp = arr[i];
             j = i;
             while ((j > 0) && arr[j-1] > temp) {
@@ -18,14 +18,24 @@ public class InsertionSort {
             arr[j] = temp;
 
             System.out.println("반복 -" + i);
+            printSort(arr, count);
         }
 
+    }
+
+    public static void printSort(int arr[], int count){
+
+        int i = 0;
+        for ( i = 0; i < count; i++){
+            System.out.println(arr[i] + "\t");
+        }
+        System.out.println();
 
     }
 
 
     //problem
-    public void main(String[] args){
+    public static void main(String[] args){
         int[] arr = {80, 50, 70, 10, 60, 20, 40, 30};
         insertionSort(arr, 8);
 
