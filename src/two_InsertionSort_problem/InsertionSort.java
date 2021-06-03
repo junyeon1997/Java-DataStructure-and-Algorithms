a53b081d8bcddae4ec02cb213f98629419b6edf8
junyeon1997 <junyeon2012@gmail.com>
2021-06-03 12:33:36 +0900

    Update insertionsort problem

---
+++ src/two_InsertionSort_problem/InsertionSort.java
@@ -0,0 +1,17 @@
+package two_InsertionSort_problem;
+
+public class InsertionSort {
+
+    public void static insertionSort(int[] arr, int count){
+
+
+    }
+
+
+    //problem
+    public void main(String[] args){
+        int[] arr = {80, 50, 70, 10, 60, 20, 40, 30};
+        insertionSort(arr, 8);
+
+    }
+}
