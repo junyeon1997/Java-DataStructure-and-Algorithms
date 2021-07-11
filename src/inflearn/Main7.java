01b7970f67bb8534d1440748c57cf5fc26ec6ff4
junyeon1997 <junyeon2012@gmail.com>
2021-07-11 14:37:46 +0900

    인프런 6.팰린드롬

---
+++ src/inflearn/Main7.java
@@ -0,0 +1,44 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        Main7 T = new Main7();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution1(str));
+    }
+
+    public String solution(String str) {
+        String answer = "";
+        String s = str.toLowerCase();
+        String r = new StringBuilder(s).reverse().toString();
+        if (s.equals(r)) {
+            answer = "YES";
+        }else{
+            answer =  "NO";
+        }
+        return answer;
+    }
+    public String solution1(String str) {
+        String answer = "";
+        String r = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(r)) { //equalsIgnoreCase : 대소문자 구분 X
+            answer = "YES";
+        }else{
+            answer =  "NO";
+        }
+        return answer;
+    }
+
+    public String solution2(String str) {
+        String answer = "YES";
+        String s = str.toLowerCase();
+        int len = s.length();
+        for(int i=0; i<len/2; i++) {
+            if (s.charAt(i)!=s.charAt(len-i-1)) return "No";
+        }
+        return answer;
+    }
+}
