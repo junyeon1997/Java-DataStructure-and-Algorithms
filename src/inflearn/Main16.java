7e1b977d12c104990016b6ce2e0c832765e0b44d
junyeon1997 <junyeon2012@gmail.com>
2021-07-22 14:38:08 +0900

    인프런(Array-배열문제) 4.피보나치 수열

---
+++ src/inflearn/Main16.java
@@ -0,0 +1,25 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main16 {
+    public static void main(String[] args) {
+        Main16 T = new Main16();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int x : T.solution(n)){
+            System.out.print(x + " ");
+
+        }
+    }
+
+    public int[] solution(int n) {
+        int[] arr = new int[n];
+        arr[0]=1;
+        arr[1]=1;
+        for(int i=0; i<n-2; i++) {
+            arr[i+2]=arr[i] + arr[i+1];
+        }
+        return arr;
+    }
+}
