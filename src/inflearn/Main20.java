a338c6122032719b80ab29f357ca2d8a714f0b70
junyeon1997 <junyeon2012@gmail.com>
2021-07-29 11:12:31 +0900

    인프런(Array-배열문제) 8.등수 구하기

---
+++ src/inflearn/Main20.java
@@ -0,0 +1,33 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main20 {
+    public static void main(String[] args) {
+        Main20 T = new Main20();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for (int x : T.solution(n, arr)){
+            System.out.print(x + " ");
+
+        }
+    }
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i=0; i<n; i++) {
+            int cnt=1;
+            for (int j=0; j<n; j++){
+                if (arr[i]<arr[j]){
+                    cnt++;
+                }
+            }
+            answer[i]=cnt;
+        }
+        return answer;
+    }
+
+}
