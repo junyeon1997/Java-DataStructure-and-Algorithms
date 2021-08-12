70d873ca5cf0297688c294c7e648ea9d417d7dc2
junyeon1997 <junyeon2012@gmail.com>
2021-08-12 13:19:42 +0900

    인프런(Tow pointers, Sliding window) 4.연속 부분수열

---
+++ src/inflearn/Main28.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Main28 {
+    public static void main(String[] args) {
+        Main28 T = new Main28();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(T.solution(n,m,arr));
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        int answer=0,sum=0,lt=0;
+        for (int rt=0; rt<n; rt++ ) {
+            sum+=arr[rt];
+            if (sum==m) answer++;
+            while(sum>=m) {
+                sum-=arr[lt++];
+                if (sum==m) answer++;
+            }
+        }
+        return answer;
+    }
+
+}
